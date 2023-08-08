e93f8fee38027067d83a80589068aec53e97686c
Jaeuk Oh <wooko5@naver.com>
2023-08-08 16:41:01 +0900

    주문 검색 기능 개발 진행중

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -0,0 +1,6 @@
+package jpabook.jpashop.domain;
+
+public class OrderSearch {
+    private String memberName; //회원이름
+    private OrderStatus orderStatus; //주문상태[ORDER, CANCLE]
+}
--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,17 +1,17 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
-    private final EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public void save(Order order) {
         entityManager.persist(order);
@@ -28,7 +28,7 @@ public class OrderRepository {
     }
 
     /* N+1 테스트용으로 잠시 해둠 */
-    public List<Order> findAll(){
+    public List<Order> findAll() {
         return entityManager.createQuery("SELECT m FROM Order m").getResultList();
     }
 }
